5ed1b549aaf02d77bd373636081fefc8d41af552
Ign Capital <ign.capitals@gmail.com>
2026-01-15 09:55:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2789836448-56d8a",
+  "appId": "1:214200674117:web:32d7052f780261cae3c02c",
+  "apiKey": "AIzaSyCS1ze2KCAQ95kWFpTcBMPbPY97PL1mFbQ",
+  "authDomain": "studio-2789836448-56d8a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "214200674117"
+};
